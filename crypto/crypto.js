b02e8ecb3ba12e864722156a1cd72dcbe771b76d
Justin Thomas <justin@justinthomas.name>
2012-03-27 15:07:09 -0700

    entropy

--- crypto/crypto.js
+++ crypto/crypto.js
@@ -9,11 +9,11 @@ function update() {
   var progress = sjcl.random.getProgress(10);
 
   if(progress === undefined) {
-    e.innerHTML = "complete";
+    e.innerHTML = "sufficient";
     sjcl.random.stopCollectors();
     window.removeEventListener("mousemove",update);
   } else {
-    e.innerHTML = progress;
+    e.innerHTML = progress.toFixed(2);
   }
 }
 
